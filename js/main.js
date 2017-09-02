e58f1f71eddb65854aef489ff16ca9b904bde168
nidran <nidran@github.com>
2017-09-02 15:31:35 +0530

    My site ready for deployment.

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+  var sources=["the-times-of-india","the-telegraph", "the-verge", "bloomberg", "cnn","hacker-news","mashable","mirror", "recode"]
+  var $outerUl=$('#outerUl');
+  var $orders=$('#orders');
+  
+  var key='1741b22c28f6458392dedd92c33cb4e7';
+  $.each(sources, function(index, value)  {
+      // requrl='https://newsapi.org/v1/articles?source='+value+'&apiKey='+key;
+      $outerUl.append('<li>'+value+'</li>');    
+});
+
+  $.each(sources, function(index, value)  {
+      requrl='https://newsapi.org/v1/articles?source='+value+'&apiKey='+key;
+      // $outerUl.append('<li><h2>'+value+'</h2></li>');    
+
+     
+      requestJSON(requrl, function(json) {  
+      
+        $.each(json['articles'], function(index, value){
+
+          // console.log(value);
+          var author=value.author;
+          var title=value.title;
+          var description=value.description;
+          var url=value.url;
+          var publishedAt=value.publishedAt;
+          var imgUrl=value.urlToImage;
+          
+          $orders.append('<li> <img src="'+imgUrl+'"</img> <h3><a href="'+url+'">'+title+'</a><br></h3><h4><br>'+author+'</h4><h5><br>'+description+'</h5></br>');
+
+        })
+        
+  
+    });
+    
+        
+ }); // end requestJSON Ajax call
+   // end click event handler
+  
+  function requestJSON(url, callback) {
+    $.ajax({
+      url: url,
+      complete: function(xhr) {
+        callback.call(null, xhr.responseJSON);
+      }
+    });
+  }
+});
