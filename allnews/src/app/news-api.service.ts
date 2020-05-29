63570d288fc3cd25e2847cce9d280dc4efb52194
nidran <nidhi.ranjan145@gmail.com>
2020-05-29 17:36:58 +0530

    upgraded to use angular

---
+++ allnews/src/app/news-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = '0912abe91b6e44f5bf9cdc532d21783f';
+
+  constructor(private http:HttpClient) { }
+  initSources(){
+     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+} 
